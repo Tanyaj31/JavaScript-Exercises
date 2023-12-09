088ac2b3236b311981458076264afb6110be9bd3
Tanyaj31 <Tanya.Jha@metropolia.fi>
2023-12-09 19:43:13 +0200

    Adding Module 1

---
+++ Module 1/Ex 1.9/PrimeNumFinder.js
@@ -0,0 +1,20 @@
+function findPrime() {
+    var number = parseInt(prompt("Enter an integer:"));
+    var isPrime = true;
+
+    if (number < 2) {
+        isPrime = false;
+    } else {
+        for (var i = 2; i <= Math.sqrt(number); i++) {
+            if (number % i === 0) {
+                isPrime = false;
+                break;
+            }
+        }
+    }
+
+    var message = isPrime ? number + " is a prime number." : number + " is not a prime number.";
+    document.getElementById("answer").innerHTML = message;
+}
+
+window.onload = findPrime;

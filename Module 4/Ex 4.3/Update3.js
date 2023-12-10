e57f2c1ea22b8d94708fe7a0f47f89156aa8fe75
Tanyaj31 <Tanya.Jha@metropolia.fi>
2023-12-10 02:00:35 +0200

    Adding Module 4

---
+++ Module 4/Ex 4.3/Update3.js
@@ -0,0 +1,51 @@
+document.addEventListener("DOMContentLoaded", function () {
+    const form = document.getElementById('searchForm');
+    const resultsContainer = document.getElementById('results');
+
+    form.addEventListener('submit', function (event) {
+        event.preventDefault();
+
+        const query = document.getElementById('query').value;
+        if (query.trim() !== '') {
+            fetch(`https://api.tvmaze.com/search/shows?q=${query}`)
+                .then(response => response.json())
+                .then(data => {
+                    displayResults(data);
+                })
+                .catch(error => console.error('Error:', error));
+        }
+    });
+
+    function displayResults(tvShows) {
+        //removing old results
+        resultsContainer.innerHTML = '';
+
+        tvShows.forEach(tvShow => {
+            const article = document.createElement('article');
+
+            const titleElement = document.createElement('h2');
+            titleElement.textContent = tvShow.show.name;
+
+            const linkElement = document.createElement('a');
+            linkElement.href = tvShow.show.url;
+            linkElement.target = "_blank";
+            linkElement.textContent = 'Details';
+
+            const imageElement = document.createElement('img');
+            imageElement.src = tvShow.show.image?.medium || 'default_image.jpg';
+            imageElement.alt = tvShow.show.name;
+
+            const summaryElement = document.createElement('div');
+            summaryElement.innerHTML = tvShow.show.summary;
+
+            //appending elements to the article
+            article.appendChild(titleElement);
+            article.appendChild(linkElement);
+            article.appendChild(imageElement);
+            article.appendChild(summaryElement);
+
+            //appending the article to the results
+            resultsContainer.appendChild(article);
+        });
+    }
+});
